ba67041a35a5bac278cf0160cefe76d5d47128f9
Bulat <11gab12345@gmail.com>
2018-03-04 21:23:32 +0300

    js

--- js2/week1/exercise/index.js
+++ js2/week1/exercise/index.js
@@ -4,15 +4,39 @@ module.exports = Collection;
  * Конструктор коллекции
  * @constructor
  */
-function Collection() {}
+
+function Collection() {
+    if(arguments[0] !== undefined){
+        this.data = arguments[0]
+    }
+    else {
+        this.data = []
+    }
+}
 
 
 // Методы коллекции
-Collection.prototype.values = function () {};
+Collection.prototype.values = function () {
+    return this.data
+};
 // другие методы
 
+Collection.prototype.append = function () {
+    if(!(arguments[0] instanceof Collection))
+         this.data.push(arguments[0])
+};
+Collection.prototype.at = function () {
+    return Object.keys(this.content)[arguments[0]]
+};
+Collection.prototype.removeAt = function () {};
+
+Collection.prototype.count = function () {
+    return this.data.length
+};
 
 /**
  * Создание коллекции из массива значений
  */
-Collection.from = function () {};
+Collection.from = function () {
+    return new Collection(arguments[0])
+};
